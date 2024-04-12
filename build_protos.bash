98ecc4138223ede0d1609cb2cd4d51f96194817b
Tully Foote <tullyfoote@intrinsic.ai>
2024-04-12 18:23:35 +0000

    initial build on iron building protos

---
+++ build_protos.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Run this in rocker --home --user
+
+set -e
+
+export DISTRO=iron
+
+sudo apt-get install python3-rosinstall-generator python3-vcstool
+
+mkdir -p ${DISTRO}/src
+
+# get all of rosdistro and msg packages
+rosinstall_generator --rosdistro ${DISTRO} --upstream-devel --format repos sensor_msgs --deps > ${DISTRO}_upstream.repos
+vcs import --input ${DISTRO}_upstream.repos ${DISTRO}/src --skip-existing
+
+
+#proto specific content
+vcs import --input ${DISTRO}_proto.repos ${DISTRO}/src --force
+
+sudo rosdep init || true
+rosdep update
+
+
+
+cd ${DISTRO}
+rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
+
+colcon build
