50be1947e4126dcace540e72605e6f773d688281
Tully Foote <tullyfoote@intrinsic.ai>
2024-04-12 23:47:28 +0000

    do checkout before results generation
    Signed-off-by: Tully Foote <tullyfoote@intrinsic.ai>

--- commit_protos.bash
+++ commit_protos.bash
@@ -10,6 +10,13 @@ mkdir -p results
 
 protos=$(find iron/install/*/include -name '*.proto')
 
+git checkout generated_protos
+
+# Set git config if unset
+git config user.name || git config user.name "Automatic Update"
+git config user.email || git config user.email "tullyfoote@intrinsic.ai"
+
+
 for proto in $protos; do
 
     echo "Proto is at"
@@ -22,11 +29,6 @@ for proto in $protos; do
 
 done
 
-git checkout generated_protos
-
-# Set git config if unset
-git config user.name || git config user.name "Automatic Update"
-git config user.email || git config user.email "tullyfoote@intrinsic.ai"
 
 git add results
 git commit -m"Updating protos for distro ${DISTRO}"
