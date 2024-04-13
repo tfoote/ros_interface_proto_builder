f5eec984d3d6b8e97eacbfda8b8ddc559233ab59
Tully Foote <tullyfoote@intrinsic.ai>
2024-04-13 08:14:10 +0000

    update package list

--- build_protos.bash
+++ build_protos.bash
@@ -5,7 +5,7 @@
 set -e
 
 export DISTRO=iron
-TARGET_PACKAGES="sensor_msgs geometry_msgs"
+TARGET_PACKAGES="common_interfaces can_msgs aruco_msgs control_msgs pcl_msgs nav2_msgs"
 
 sudo apt-get update && sudo apt-get install -qy python3-rosinstall-generator python3-vcstool
 
