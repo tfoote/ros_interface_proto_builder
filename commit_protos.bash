8da9dc2b58c49aede6bef437f7185ebac1cea3ac
Tully Foote <tullyfoote@intrinsic.ai>
2024-04-22 15:31:32 -0700

    pulling LICENSE from src (#6)
    * pulling LICENSE from src

--- commit_protos.bash
+++ commit_protos.bash
@@ -29,6 +29,25 @@ for proto in $protos; do
 
 done
 
+package_dirs=$(colcon list -p)
+
+for pd in $package_dirs; do
+    license_file=$pd/LICENSE
+    echo Trying License file: $license_file
+    if ! [ -f $license_file ] ; then
+        license_file=$(dirname $pd)/LICENSE
+    fi
+    if ! [ -f $license_file ] ; then
+	echo NO license file at $license_file either, continuing
+	continue
+    fi
+    package=$(basename $pd)
+    result_dir=results/${ROS_DISTRO}/${package}/
+    if [ -d $result_dir ] ; then
+	cp $license_file $result_dir
+    fi
+done
+
 echo "Git adding results directory"
 git add results
 if ! git diff --cached --exit-code
